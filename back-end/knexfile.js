a4f81df70f8e3ff81db5e7fdceb5e96b39dc62b1
Grapes1875 <alexreyes1875@gmail.com>
2024-06-19 17:08:15 -0500

    Fixed knex.js file

--- back-end/knexfile.js
+++ back-end/knexfile.js
@@ -1,4 +1,4 @@
-const path = require("path");
+const path = require('path');
 
 const {
   DATABASE_URL,
@@ -10,50 +10,50 @@ const {
 
 module.exports = {
   development: {
-    client: "postgresql",
-    pool: { min: 1, max: 5 },
+    client: 'postgresql',
     connection: DATABASE_URL_DEVELOPMENT,
+    pool: { min: 1, max: 5 },
     migrations: {
-      directory: path.join(__dirname, "src", "db", "migrations"),
+      directory: path.join(__dirname, 'src', 'db', 'migrations'),
     },
     seeds: {
-      directory: path.join(__dirname, "src", "db", "seeds"),
+      directory: path.join(__dirname, 'src', 'db', 'seeds'),
     },
     debug: !!DEBUG,
   },
   test: {
-    client: "postgresql",
-    pool: { min: 1, max: 5 },
+    client: 'postgresql',
     connection: DATABASE_URL_TEST,
+    pool: { min: 1, max: 5 },
     migrations: {
-      directory: path.join(__dirname, "src", "db", "migrations"),
+      directory: path.join(__dirname, 'src', 'db', 'migrations'),
     },
     seeds: {
-      directory: path.join(__dirname, "src", "db", "seeds"),
+      directory: path.join(__dirname, 'src', 'db', 'seeds'),
     },
     debug: !!DEBUG,
   },
   preview: {
-    client: "postgresql",
-    pool: { min: 1, max: 5 },
+    client: 'postgresql',
     connection: DATABASE_URL_PREVIEW,
+    pool: { min: 1, max: 5 },
     migrations: {
-      directory: path.join(__dirname, "src", "db", "migrations"),
+      directory: path.join(__dirname, 'src', 'db', 'migrations'),
     },
     seeds: {
-      directory: path.join(__dirname, "src", "db", "seeds"),
+      directory: path.join(__dirname, 'src', 'db', 'seeds'),
     },
     debug: !!DEBUG,
   },
   production: {
-    client: "postgresql",
-    pool: { min: 1, max: 5 },
+    client: 'postgresql',
     connection: DATABASE_URL,
+    pool: { min: 1, max: 5 },
     migrations: {
-      directory: path.join(__dirname, "src", "db", "migrations"),
+      directory: path.join(__dirname, 'src', 'db', 'migrations'),
     },
     seeds: {
-      directory: path.join(__dirname, "src", "db", "seeds"),
+      directory: path.join(__dirname, 'src', 'db', 'seeds'),
     },
     debug: !!DEBUG,
   },
