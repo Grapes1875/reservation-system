ebaf786a097620ac9a4838bca402d12c0d240e08
Grapes1875 <alexreyes1875@gmail.com>
2024-06-19 16:11:10 -0500

    fixed connection.js

--- back-end/src/db/connection.js
+++ back-end/src/db/connection.js
@@ -1,5 +1,15 @@
-const environment = process.env.NODE_ENV || "development";
-const config = require("../../knexfile")[environment];
-const knex = require("knex")(config);
+const environment = process.env.NODE_ENV || 'development';
+const config = require('../../knexfile')[environment];
+
+// Modify the Knex initialization to include SSL settings
+const knex = require('knex')({
+  client: 'pg', // Assuming you're using PostgreSQL
+  connection: {
+    connectionString: config.connection,
+    ssl: {
+      rejectUnauthorized: false, // This setting depends on your PostgreSQL setup
+    },
+  },
+});
 
 module.exports = knex;
