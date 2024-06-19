6c74006658ffb0c7005cff601da8aaf0d876a419
Grapes1875 <alexreyes1875@gmail.com>
2024-06-19 17:37:57 -0500

    Going back to the if else statements maybe itll work

--- back-end/knexfile.js
+++ back-end/knexfile.js
@@ -1,10 +1,8 @@
 const path = require('path');
 
-module.exports = {
-
-  development: {
+module.exports = (environment) => {
+  const config = {
     client: 'postgresql',
-    connection: process.env.DATABASE_URL_DEVELOPMENT || process.env.POSTGRES_URL_DEVELOPMENT,
     migrations: {
       directory: path.join(__dirname, 'src', 'db', 'migrations'),
     },
@@ -12,30 +10,17 @@ module.exports = {
       directory: path.join(__dirname, 'src', 'db', 'seeds'),
     },
     debug: process.env.DEBUG === 'true',
-  },
+  };
 
-  test: {
-    client: 'postgresql',
-    connection: process.env.DATABASE_URL_TEST || process.env.POSTGRES_URL_TEST,
-    migrations: {
-      directory: path.join(__dirname, 'src', 'db', 'migrations'),
-    },
-    seeds: {
-      directory: path.join(__dirname, 'src', 'db', 'seeds'),
-    },
-    debug: process.env.DEBUG === 'true',
-  },
+  if (environment === 'development') {
+    config.connection = process.env.DATABASE_URL_DEVELOPMENT || process.env.POSTGRES_URL_DEVELOPMENT;
+  } else if (environment === 'test') {
+    config.connection = process.env.DATABASE_URL_TEST || process.env.POSTGRES_URL_TEST;
+  } else if (environment === 'production') {
+    config.connection = process.env.DATABASE_URL || process.env.POSTGRES_URL;
+  } else {
+    throw new Error('Environment not properly set.');
+  }
 
-  production: {
-    client: 'postgresql',
-    connection: process.env.DATABASE_URL || process.env.POSTGRES_URL,
-    migrations: {
-      directory: path.join(__dirname, 'src', 'db', 'migrations'),
-    },
-    seeds: {
-      directory: path.join(__dirname, 'src', 'db', 'seeds'),
-    },
-    debug: process.env.DEBUG === 'true',
-  },
-
+  return config;
 };
