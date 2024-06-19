ceaf153e1330e267b2e07b1835389fec10b90a7d
Grapes1875 <alexreyes1875@gmail.com>
2024-06-19 17:20:12 -0500

    Fixed knexfile structure

--- back-end/knexfile.js
+++ back-end/knexfile.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   development: {
     client: 'postgresql',
-    connection: process.env.POSTGRES_URL_NON_POOLING || process.env.DATABASE_URL_DEVELOPMENT,
+    connection: process.env.POSTGRES_URL_DEVELOPMENT || process.env.DATABASE_URL_DEVELOPMENT,
     migrations: {
       directory: path.join(__dirname, 'src', 'db', 'migrations'),
     },
@@ -14,18 +14,7 @@ module.exports = {
   },
   test: {
     client: 'postgresql',
-    connection: process.env.POSTGRES_URL_NO_SSL || process.env.DATABASE_URL_TEST,
-    migrations: {
-      directory: path.join(__dirname, 'src', 'db', 'migrations'),
-    },
-    seeds: {
-      directory: path.join(__dirname, 'src', 'db', 'seeds'),
-    },
-    debug: process.env.DEBUG === 'true',
-  },
-  preview: {
-    client: 'postgresql',
-    connection: process.env.POSTGRES_PRISMA_URL || process.env.DATABASE_URL_PREVIEW,
+    connection: process.env.POSTGRES_URL_TEST || process.env.DATABASE_URL_TEST,
     migrations: {
       directory: path.join(__dirname, 'src', 'db', 'migrations'),
     },
