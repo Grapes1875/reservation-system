28fd5d2e3643cf1d7496a06893f5b800637a1d1b
Grapes1875 <alexreyes1875@gmail.com>
2024-06-19 17:33:27 -0500

    Fixed knexfile.js again

--- back-end/knexfile.js
+++ back-end/knexfile.js
@@ -1,18 +1,10 @@
 const path = require('path');
 
 module.exports = {
+
   development: {
     client: 'postgresql',
-    connection: process.env.DATABASE_URL_DEVELOPMENT || {
-      user: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DATABASE,
-      host: process.env.POSTGRES_HOST,
-      port: process.env.POSTGRES_PORT || 5432,
-      ssl: {
-        rejectUnauthorized: false, // Use only for local development if self-signed SSL certificate is used
-      },
-    },
+    connection: process.env.DATABASE_URL_DEVELOPMENT || process.env.POSTGRES_URL_DEVELOPMENT,
     migrations: {
       directory: path.join(__dirname, 'src', 'db', 'migrations'),
     },
@@ -21,18 +13,10 @@ module.exports = {
     },
     debug: process.env.DEBUG === 'true',
   },
+
   test: {
     client: 'postgresql',
-    connection: process.env.DATABASE_URL_TEST || {
-      user: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DATABASE,
-      host: process.env.POSTGRES_HOST,
-      port: process.env.POSTGRES_PORT || 5432,
-      ssl: {
-        rejectUnauthorized: false, // Use only for local development if self-signed SSL certificate is used
-      },
-    },
+    connection: process.env.DATABASE_URL_TEST || process.env.POSTGRES_URL_TEST,
     migrations: {
       directory: path.join(__dirname, 'src', 'db', 'migrations'),
     },
@@ -41,18 +25,10 @@ module.exports = {
     },
     debug: process.env.DEBUG === 'true',
   },
+
   production: {
     client: 'postgresql',
-    connection: process.env.DATABASE_URL || {
-      user: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DATABASE,
-      host: process.env.POSTGRES_HOST,
-      port: process.env.POSTGRES_PORT || 5432,
-      ssl: {
-        rejectUnauthorized: true, // Always use true for production to enforce SSL
-      },
-    },
+    connection: process.env.DATABASE_URL || process.env.POSTGRES_URL,
     migrations: {
       directory: path.join(__dirname, 'src', 'db', 'migrations'),
     },
@@ -61,4 +37,5 @@ module.exports = {
     },
     debug: process.env.DEBUG === 'true',
   },
+
 };
