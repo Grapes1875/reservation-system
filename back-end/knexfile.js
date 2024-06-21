86322bed21d7a6c4ff627da71be15a1c92295c83
Grapes1875 <alexreyes1875@gmail.com>
2024-06-21 09:22:24 -0500

    Fixing Server error

--- back-end/knexfile.js
+++ back-end/knexfile.js
@@ -1,12 +1,9 @@
 const path = require('path');
 
-module.exports = (environment) => {
-  if (!environment) {
-    throw new Error('Environment not properly set.');
-  }
-
-  const config = {
+module.exports = {
+  development: {
     client: 'postgresql',
+    connection: process.env.DATABASE_URL_DEVELOPMENT,
     migrations: {
       directory: path.join(__dirname, 'src', 'db', 'migrations'),
     },
@@ -14,17 +11,27 @@ module.exports = (environment) => {
       directory: path.join(__dirname, 'src', 'db', 'seeds'),
     },
     debug: process.env.DEBUG === 'true',
-  };
-
-  if (environment === 'development') {
-    config.connection = process.env.DATABASE_URL_DEVELOPMENT || process.env.POSTGRES_URL_DEVELOPMENT;
-  } else if (environment === 'test') {
-    config.connection = process.env.DATABASE_URL_TEST || process.env.POSTGRES_URL_TEST;
-  } else if (environment === 'production') {
-    config.connection = process.env.DATABASE_URL || process.env.POSTGRES_URL;
-  } else {
-    throw new Error('Environment not properly set.');
-  }
-
-  return config;
+  },
+  test: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL_TEST,
+    migrations: {
+      directory: path.join(__dirname, 'src', 'db', 'migrations'),
+    },
+    seeds: {
+      directory: path.join(__dirname, 'src', 'db', 'seeds'),
+    },
+    debug: process.env.DEBUG === 'true',
+  },
+  production: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: path.join(__dirname, 'src', 'db', 'migrations'),
+    },
+    seeds: {
+      directory: path.join(__dirname, 'src', 'db', 'seeds'),
+    },
+    debug: process.env.DEBUG === 'true',
+  },
 };
